c42f9402aadbc5520c1e8e2f3478e2d6933b438d
PaoloDiaz99 <pda99@hotmail.com>
2024-08-11 20:15:40 -0500

    Version1

---
+++ src/index.mjs
@@ -0,0 +1,85 @@
+import mysql from 'mysql';
+
+const con = mysql.createConnection({
+  host: 'prueba-lambda.cx0ia8us6v3j.sa-east-1.rds.amazonaws.com',
+  user: 'admin',
+  port: "3306",
+  password: 'xxxxxxx',
+  database: 'database',
+});
+
+
+export const handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const query = (sql, values) => new Promise((resolve, reject) => {
+    con.query(sql, values, (err, res) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(res);
+    });
+  });
+
+  try {
+    // Detectar el método HTTP
+    switch (event.httpMethod) {
+      case 'POST': {
+        const { estado } = JSON.parse(event.body);
+        const sql = "INSERT INTO tb_estado (estado) VALUES (?)";
+        await query(sql, [estado]);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Se registró el valor.' }),
+        };
+      }
+
+      case 'GET': {
+        const sql = "SELECT * FROM tb_estado";
+        const results = await query(sql);
+        return {
+          statusCode: 200,
+          body: JSON.stringify(results),
+        };
+      }
+
+      case 'PUT': {
+        const { id_estado, estado } = JSON.parse(event.body);
+        const sql = "UPDATE tb_estado SET estado = ? WHERE id_estado = ?";
+        await query(sql, [estado, id_estado]);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Se actualizó el valor.' }),
+        };
+      }
+
+      case 'DELETE': {
+        const id_estado = event.queryStringParameters?.id_estado;
+        if (!id_estado) {
+          return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "El ID del estado es requerido." }),
+          };
+        }
+        const sql = "DELETE FROM tb_estado WHERE id_estado = ?";
+        await query(sql, [id_estado]);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Se eliminó el valor.' }),
+        };
+      }
+
+      default: {
+        return {
+          statusCode: 405,
+          body: JSON.stringify({ message: 'Método no permitido.' }),
+        };
+      }
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Error: ' + err.message }),
+    };
+  }
+};
